09b83293d0fe96f390e79f710aa4d68f2e6e6def
DariuszKitel <dariuszkitel@gmail.com>
2020-10-26 03:10:00 +0100

    Added general repository and specialized repository for category model

---
+++ BookShop/BookShop.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.DataAccess.Repository.IRepository
+{
+    interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
